406372c363c5a68c6871c52e9b68ea777c2a876f
Bill Hughes <bhughesatl@gmail.com>
2023-09-12 18:59:03 -0700

    Initials Project

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Bill Hughes
+# I am a speedrunner
+print("B B B B      H       H")
+print("B       B    H       H")
+print("B       B    H       H")
+print("B B B B      H H H H H")
+print("B       B    H       H")
+print("B       B    H       H")
+print("B B B B      H       H")
